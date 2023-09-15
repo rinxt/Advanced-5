943b5600b2c10f2feace3557210aa7e488d0eace
Мамаева Арина Евгеньевна <m18282@kometadom.local>
2023-09-15 15:34:57 +0300

    Task3 added

---
+++ Task3/Task3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class DivisibleByThreeCounter {
+private:
+    int sum;
+    int count;
+
+public:
+    DivisibleByThreeCounter() : sum(0), count(0) {}
+
+    void operator()(int num) {
+        if (num % 3 == 0) {
+            sum += num;
+            count++;
+        }
+    }
+
+    int get_sum() {
+        return sum;
+    }
+
+    int get_count() {
+        return count;
+    }
+};
+
+int main() {
+    std::vector<int> numbers;
+
+    std::srand((std::time(nullptr)));
+    for (int i = 0; i < 10; ++i) {
+        numbers.push_back(std::rand() % 11);
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    DivisibleByThreeCounter counter = std::for_each(numbers.begin(), numbers.end(), DivisibleByThreeCounter());
+
+    std::cout << "get_sum() = " << counter.get_sum() << std::endl;
+    std::cout << "get_count() = " << counter.get_count() << std::endl;
+
+    return 0;
+}
